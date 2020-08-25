d4a195f077f1bd2a351049cb10dc5f3067e48bdd
flywheelnz <colin@flywheel.nz>
2020-08-25 13:01:47 +1200

    Add support for efs_volume_configuration

--- modules/core/task/main.tf
+++ modules/core/task/main.tf
@@ -33,13 +33,16 @@ resource "aws_ecs_task_definition" "main" {
       dynamic "efs_volume_configuration" {
         for_each = lookup(volume.value, "efs_volume_configuration", null) == null ? [] : [volume.value["efs_volume_configuration"]]
         content {
-          file_system_id      = lookup(efs_volume_configuration.value, "file_system_id", null)
-          root_directory      = lookup(efs_volume_configuration.value, "root_directory", null)
-          transit_encryption  = lookup(efs_volume_configuration.value, "transit_encryption", "ENABLED")
-          transit_encryption  = lookup(efs_volume_configuration.value, "transit_encryption", 2999)
-          authorization_config {
-            access_point_id   = lookup(efs_volume_configuration.value, "authorization_config.access_point_id", 2999)
-            iam               = lookup(efs_volume_configuration.value, "authorization_config.iam", "ENABLED")
+          file_system_id          = lookup(efs_volume_configuration.value, "file_system_id", null)
+          root_directory          = lookup(efs_volume_configuration.value, "root_directory", null)
+          transit_encryption      = lookup(efs_volume_configuration.value, "transit_encryption", "ENABLED")
+          transit_encryption_port = lookup(efs_volume_configuration.value, "transit_encryption_port", null)
+          dynamic "authorization_config" {
+            for_each = lookup(efs_volume_configuration.value, "authorization_config", null) == null ? [] : [efs_volume_configuration.value["authorization_config"]]
+            content {
+              access_point_id       = lookup(authorization_config.value, "access_point_id", null)
+              iam                   = lookup(authorization_config.value, "iam", "DISABLED")
+            }
           }
         }
       }
