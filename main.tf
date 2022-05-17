d190da1f6f8c8434540f08b4adf1366cec44b877
Shinya Matsuura <shinya.matsuura@hennge.com>
2022-05-17 10:06:18 +0900

    Extract capacity provider resources with aws_ecs_cluster_capacity_providers

--- main.tf
+++ main.tf
@@ -1,17 +1,6 @@
 resource "aws_ecs_cluster" "main" {
   name = var.name
 
-  capacity_providers = var.capacity_providers
-
-  dynamic "default_capacity_provider_strategy" {
-    for_each = var.default_capacity_provider_strategy
-    content {
-      capacity_provider = lookup(default_capacity_provider_strategy.value, "capacity_provider", null)
-      weight            = lookup(default_capacity_provider_strategy.value, "weight", null)
-      base              = lookup(default_capacity_provider_strategy.value, "base", null)
-    }
-  }
-
   dynamic "setting" {
     for_each = var.settings
     content {
@@ -33,4 +22,21 @@ resource "aws_ecs_cluster" "main" {
   }
 
   tags = var.tags
+}
+
+resource "aws_ecs_cluster_capacity_providers" "capacity_provider" {
+  count = var.capacity_providers != null || var.default_capacity_provider_strategy != [] ? 1 : 0
+
+  cluster_name = aws_ecs_cluster.main.name
+
+  capacity_providers = var.capacity_providers
+
+  dynamic "default_capacity_provider_strategy" {
+    for_each = var.default_capacity_provider_strategy
+    content {
+      capacity_provider = lookup(default_capacity_provider_strategy.value, "capacity_provider", null)
+      weight            = lookup(default_capacity_provider_strategy.value, "weight", null)
+      base              = lookup(default_capacity_provider_strategy.value, "base", null)
+    }
+  }
 }
