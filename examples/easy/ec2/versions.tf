03fff2ea84938f9f49dacab9bdb37029c2298bba
Hans Gunawan <hans.gunawan@hennge.com>
2022-05-18 11:24:06 +0900

    Set min TF ver to 0.13.1, min provider ver to 3.74.0

--- examples/easy/ec2/versions.tf
+++ examples/easy/ec2/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 0.13.1"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.35.0"
+      version = ">= 3.74.0"
     }
   }
 }
