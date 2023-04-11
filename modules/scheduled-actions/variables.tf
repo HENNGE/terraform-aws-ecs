fc602f15fce5d1882f41ac7e73d6b2fa49fc3019
Muhammad Furqan Habibi <furqan.habibi@hennge.com>
2023-04-11 15:51:30 +0900

    Add capacity provider strategy to scheduled action

--- modules/scheduled-actions/variables.tf
+++ modules/scheduled-actions/variables.tf
@@ -63,3 +63,9 @@ variable "container_overrides" {
   default     = null
   type        = string
 }
+
+variable "capacity_provider_strategy" {
+  description = "The capacity provider strategy to use for the task. See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_event_target#capacity_provider_strategy"
+  default     = null
+  type        = map(any)
+}
