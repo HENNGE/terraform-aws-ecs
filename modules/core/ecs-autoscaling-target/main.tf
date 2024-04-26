d87bd2e06f957b1587aa788ca1aa749cdcdbae56
Muhammad Furqan Habibi <furqan.habibi@hennge.com>
2024-04-26 10:47:04 +0900

    Fix provider default tags tf plan pollution

--- modules/core/ecs-autoscaling-target/main.tf
+++ modules/core/ecs-autoscaling-target/main.tf
@@ -1,6 +1,11 @@
 resource "aws_appautoscaling_target" "ecs_target" {
   count = var.ignore_capacity_changes ? 0 : 1
 
+  # Workaround for https://github.com/hashicorp/terraform-provider-aws/issues/31261
+  lifecycle {
+    ignore_changes = [tags_all]
+  }
+
   min_capacity       = var.min_capacity
   max_capacity       = var.max_capacity
   resource_id        = "service/${var.ecs_cluster_name}/${var.ecs_service_name}"
@@ -11,8 +16,9 @@ resource "aws_appautoscaling_target" "ecs_target" {
 resource "aws_appautoscaling_target" "ecs_target_ignore_capacity_changes" {
   count = var.ignore_capacity_changes ? 1 : 0
 
+  # Workaround for https://github.com/hashicorp/terraform-provider-aws/issues/31261
   lifecycle {
-    ignore_changes = [min_capacity, max_capacity]
+    ignore_changes = [min_capacity, max_capacity, tags_all]
   }
 
   min_capacity       = var.min_capacity
